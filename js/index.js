b7408ffa37c434e09f3afabbddd392b59c2ee58f
AlexMinez <alex_minihan@outlook.com>
2022-04-28 02:05:16 +1000

    Done.

---
+++ js/index.js
@@ -0,0 +1,42 @@
+
+
+const newForm = document.getElementById('github-form')
+
+newForm.addEventListener('submit', function (e){
+    e.preventDefault();
+
+    const search = document.getElementById("search").value
+
+    return fetch('https://api.github.com/users/'+search)
+        .then (res => res.json())
+        .then (data => {
+
+            document.getElementById("github-container").innerHTML = `<img src="${data.avatar_url}"/>`
+
+            const list = document.getElementById('github-container')
+            const li = document.createElement('li')
+            
+            li.innerHTML = `<a href="${data.html_url}" target="_blank">Link to profile!</a>
+            <br><br>
+            Username : ${data.login} 
+            <br><br> 
+            <button id='repoBtn'> Click to view this persons Repo's </button>
+            <br><br>
+            `
+            list.appendChild(li)
+
+            
+            repoBtn.addEventListener('click', function(){
+                return fetch('https://api.github.com/users/AlexMinez/repos')
+                .then(res => res.json())
+                .then (data => {
+              
+                    console.log(data)
+                const repoList = document.getElementById('github-container')
+                const li2 = document.createElement('li')
+                        li2.innerHTML = `${data.full_name}`
+                        repoList.appendChild(li2)
+      })
+    })                                 
+  })     
+})
